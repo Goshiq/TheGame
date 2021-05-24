83c90a85005c4946ea11b3af57ee95d9d87cdacd
Jerald Mogo <jmogo@et-k4.kzn.21-school.ru>
2021-05-24 16:26:01 +0300

    just to check

--- src/main/java/com/thegame/Main.java
+++ src/main/java/com/thegame/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         try {
             MyMap   map = new MyMap();
-            map.printMap();
+            
         }
         catch (Exception e) {
             System.out.println(e);
